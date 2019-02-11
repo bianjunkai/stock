10f5c49e9889f3c9634f201331a1f439207e4ba8
bianjunkai <bianjunkai@gmail.com>
2019-02-11 15:31:01 +0800

    get finaical information

--- stock_fin.py
+++ stock_fin.py
@@ -1,42 +1,31 @@
 # -*- coding: UTF-8 -*-
-import tushare as ts
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer,VARCHAR
 from sqlalchemy.orm import sessionmaker
-import pymysql
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import  String,Column,Integer,VARCHAR
+import time
+import tushare as ts
 
 pro = ts.pro_api()
 
-Base = declarative_base()
-class StockBase(Base):
-    # 表的名字:
-    __tablename__ = 'stock_zj_filted'
-
-    # 表的结构:
-    ts_code = Column(VARCHAR(255),primary_key=True)
-    name = Column(VARCHAR(255))
-    area = Column(VARCHAR(255))
-    industry = Column(VARCHAR(255))
-
-
-# data = pro.stock_basic(exchange='', list_status='L', fields='ts_code,name,area,industry')
 engine = create_engine('mysql+pymysql://root:root@localhost:3306/stock_base?charset=utf8')
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Stock(Base):
+	__tablename__ = "stock_zj_filted"
+	ts_code = Column(VARCHAR(255),primary_key=True)
+	name = Column(VARCHAR(255))
+	area = Column(VARCHAR(255))
+	industry = Column(VARCHAR(255))
+
 try:
-	DBsession = sessionmaker(bind=engine)
-	session = DBsession()
-	ts_code = session.query(StockBase).all()
-	for ts in ts_code: 	
-		df_cursor = pro.query('fina_indicator',ts_code=ts,start_date='20170101',fields='end_date,q_dtprofit,q_gsprofit_margin,debt_to_assets,roe')
-#	data.to_sql('stock_location',con=engine,if_exists='replace',index=False)
-	
+	Base.metadata.create_all(engine)
+	session = Session()
+	stocks=session.query(Stock).all()
+	for stock in stocks:
+		df_cursor = pro.query('fina_indicator',ts_code=stock.ts_code,start_date='20180101',fields='ts_code,end_date,q_dtprofit,q_gsprofit_margin,debt_to_assets,roe')
+		df_cursor.to_sql('stock_zj_fin',con=engine,if_exists='append',index=False)
+		time.sleep(1)
 except Exception as e:
 	print(e)
-
-
-
-
-
-
-
-#df = pro.query('fina_indicator',ts_code='603535.SH',start_date='20170101',fields='end_date,q_dtprofit,q_gsprofit_margin,debt_to_assets,roe')
